6328a0b6e33eef535258380850ff93c1ed6d619a
Balwinder <bal@debian>
2020-02-18 17:53:05 +0530

    Pass Subscriber name as react component prop. prop name should _xrname.

--- src/jsmxq-react.js
+++ src/jsmxq-react.js
@@ -2,14 +2,26 @@ import React from 'react';
 import * as jsmxq from "jsmxq";
 
 /*Message queue Xchange*/
-var gJsmXchange = new jsmxq.Xchange();
+if(gJsmXchange === undefined) {
+    var gJsmXchange = new jsmxq.Xchange();
+}
 
 /*Xchange + React component*/
 export default class XRComponent extends React.Component {
 
-    constructor(name) {
-        super();
-        this.subscriber = new jsmxq.Subscriber(name);
+    constructor(props) {
+        super(props);
+	
+	/*
+	 * _xrname should not used in componet props as it is reserved to pass name to subscriber.
+	 * _xrname should be unique for each component. Message is passed based on this _xrname.
+	 */	
+        if(this.props !== undefined && this.props._xrname !== undefined) {
+            this.subscriber = new jsmxq.Subscriber(this.props._xrname);
+	} else {
+            this.subscriber = new jsmxq.Subscriber("_xrname_");
+	}
+
         this.subscriber.setCallbackObj(this);
         gJsmXchange.subscribe(this.subscriber);
     }
